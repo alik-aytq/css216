634d7ca4b38d5bfbf699e0200e20d1920b154122
Aitkaliev Alikhan <240118014@sdu.edu.kz>
2026-09-19 18:38:05 +0500

    HW1: Task 10

---
+++ src/Task10.kt
@@ -0,0 +1,19 @@
+fun main() {
+    printCityWeather("Ankara", 27, 31, 82)
+    printCityWeather("Tokyo", 32, 36, 10)
+    printCityWeather("Cape Town", 59, 64, 2)
+    printCityWeather("Guatemala City", 50, 55, 7)
+}
+
+fun printCityWeather(
+    cityName: String,
+    lowTemp: Int,
+    highTemp: Int,
+    chanceOfRain: Int
+) {
+    println("City: $cityName")
+    println("Low temperature: $lowTemp, High temperature: $highTemp")
+    println("Chance of rain: $chanceOfRain%")
+    println()
+}
+
