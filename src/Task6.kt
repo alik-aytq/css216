1c485e7f9b3aa3033fcf20383e566038c3adcd45
Aitkaliev Alikhan <240118014@sdu.edu.kz>
2026-09-19 18:22:48 +0500

    HW1: Task 6

---
+++ src/Task6.kt
@@ -0,0 +1,33 @@
+//fun main() {
+//    val firstNumber = 10
+//    val secondNumber = 5
+//    val result = firstNumber + secondNumber
+//    println("$firstNumber + $secondNumber = $result")
+//}
+
+fun main() {
+    val firstNumber = 10
+    val secondNumber = 5
+    val thirdNumber = 8
+
+    val result = add(firstNumber, secondNumber)
+    val anotherResult = add(firstNumber, thirdNumber)
+
+    println("$firstNumber + $secondNumber = $result")
+    println("$firstNumber + $thirdNumber = $anotherResult")
+
+    val result2 = substract(firstNumber, secondNumber)
+    val anotherResult2 = substract(firstNumber, thirdNumber)
+
+    println("$firstNumber - $secondNumber = $result2")
+    println("$firstNumber - $thirdNumber = $anotherResult2")
+}
+
+// Define add() function below this line
+fun add(firstNumber: Int, secondNumber: Int): Int {
+    return firstNumber + secondNumber
+}
+
+fun substract(firstNumber: Int, secondNumber: Int): Int {
+    return firstNumber - secondNumber
+}
