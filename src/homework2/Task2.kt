baaf094c750c6f83615ae057e816655f8664f9e6
Aitkaliev Alikhan <240118014@sdu.edu.kz>
2026-09-20 23:26:53 +0500

    HW2: Task 2

---
+++ src/homework2/Task2.kt
@@ -0,0 +1,27 @@
+package homework2
+
+fun main() {
+    val child = 5
+    val adult = 28
+    val senior = 87
+
+    val isMonday = true
+
+    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+}
+
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+    if (age>=13&&age<=60&&isMonday) {
+        return 25
+    } else if (age>=13&&age<=60) {
+        return 30
+    } else if (age>=61&&age<100){
+        return 20
+    } else if (age<=12){
+        return 15
+    } else {
+        return -1
+    }
+}
